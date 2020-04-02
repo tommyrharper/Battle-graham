c8f7885f5bb3ff735fac1c40edc7537306e77d87
tommyrharper <tommyrharper@gmail.com>
2020-04-02 17:19:07 +0100

    test failed - player cannot return their name

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,12 @@
 require 'player'
 
 describe Player do
-  
-  
+  subject(:ash) { Player.new('Ash') }
+
+  describe '#name' do
+    it 'returns the name' do
+      expect(ash.name).to eq 'Ash'
+    end
+  end
+
 end
