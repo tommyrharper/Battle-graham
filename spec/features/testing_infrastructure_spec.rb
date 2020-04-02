ba8cc8b41a6ad8b583e510c8820499b290ec1fc8
tommyrharper <tommyrharper@gmail.com>
2020-04-02 16:55:37 +0100

    tests refactored to be DRY

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -3,20 +3,14 @@ require_relative '../spec_helper'
 
 feature 'I want players to be able to enter their names' do
   scenario 'They enter their names and submit' do
-    visit('/')
-    fill_in :player_1_name, with: 'Ash Ketchum'
-    fill_in :player_2_name, with: 'Brock(rock)'
-    click_button 'submit'
+    sign_in_and_play
     expect(page).to have_content'Ash Ketchum vs Brock(rock)'
   end
 end
 
 feature 'I want to see player 2s hitpoints' do
   scenario 'viewing hitpoints' do
-    visit('/')
-    fill_in :player_1_name, with: 'Ash Ketchum'
-    fill_in :player_2_name, with: 'Brock(rock)'
-    click_button 'submit'
+    sign_in_and_play
     expect(page).to have_content'Brock(rock): HP 100'
   end
 end
