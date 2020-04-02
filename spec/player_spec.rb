d831dbbf54627077335a277396113804230e3acb
tommyrharper <tommyrharper@gmail.com>
2020-04-02 17:39:26 +0100

    test failed - no attack method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,6 +2,7 @@ require 'player'
 
 describe Player do
   subject(:ash) { Player.new('Ash') }
+  subject(:brock) { Player.new('Brock') }
 
   describe '#name' do
     it 'returns the name' do
@@ -11,7 +12,14 @@ describe Player do
 
   describe '#hp' do
     it 'returns the hp' do
-      expect(ash.hp).to eq 100
+      expect(ash.hp).to eq Player::DEFAULT_HP
+    end
+  end
+
+  describe '#attack' do
+    it 'removes 10hp from oppossing player' do
+      ash.attack(brock)
+      expect(brock.hp).to eq Player:DEFAULT_HP-10
     end
   end
 
