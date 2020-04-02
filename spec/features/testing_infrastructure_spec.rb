726e6351f1790534d860d03a35daacdf0f6c8cca
tommyrharper <tommyrharper@gmail.com>
2020-04-02 16:36:38 +0100

    test failed - HP can be not be seen

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -10,3 +10,13 @@ feature 'I want players to be able to enter their names' do
     expect(page).to have_content'Ash Ketchum vs Brock(rock)'
   end
 end
+
+feature 'I want to see player 2s hitpoints' do
+  scenario 'viewing hitpoints' do
+    visit('/')
+    fill_in :player_1_name, with: 'Ash Ketchum'
+    fill_in :player_2_name, with: 'Brock(rock)'
+    click_button 'submit'
+    expect(page).to have_content'Brock(rock): HP 100'
+  end
+end
